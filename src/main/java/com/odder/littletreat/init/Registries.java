159283f03b9669ac0eb43348f551f970dd4da4bf
Christian Brintnall <christianbrintnall@gmail.com>
2026-07-30 15:54:15 -0700

    remove unused process definition

--- src/main/java/com/odder/littletreat/init/Registries.java
+++ src/main/java/com/odder/littletreat/init/Registries.java
@@ -2,7 +2,6 @@ package com.odder.littletreat.init;
 
 import com.odder.littletreat.LittleTreat;
 import com.odder.littletreat.codec.FoodDefinition;
-import com.odder.littletreat.codec.ProcessDefinition;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
@@ -11,12 +10,8 @@ import net.neoforged.neoforge.registries.DataPackRegistryEvent;
 public class Registries {
     public static final ResourceKey<Registry<FoodDefinition>> FOOD_DEFINITIONS =
             ResourceKey.createRegistryKey(ResourceLocation.fromNamespaceAndPath(LittleTreat.MODID, "food_definitions"));
-
-    public static final ResourceKey<Registry<ProcessDefinition>> PROCESS_DEFINITIONS =
-            ResourceKey.createRegistryKey(ResourceLocation.fromNamespaceAndPath(LittleTreat.MODID, "process"));
-
+    
     public static void registerDatapackRegistries(DataPackRegistryEvent.NewRegistry event) {
         event.dataPackRegistry(FOOD_DEFINITIONS, FoodDefinition.CODEC);
-        event.dataPackRegistry(PROCESS_DEFINITIONS, ProcessDefinition.CODEC);
     }
 }
